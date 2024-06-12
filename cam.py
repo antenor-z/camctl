f8c4becabb44ebbc3794bebb03bc021e33f708f4
antenor-z <antenor-z@protonmail.com>
2024-06-12 10:03:55 -0300

    using curses

--- cam.py
+++ cam.py
@@ -1,31 +1,50 @@
+import curses
 from json import load
 from dvrip import DVRIPCam
 from time import sleep
 
-host_ip = '192.168.86.97'
+def control_camera(stdscr):
+    # Load camera configuration
+    host_ip = '192.168.86.97'
 
-with open("config.json") as fp:
-    auth = load(fp)
-cam = DVRIPCam(host_ip, user=auth["user"], password=auth["password"])
-if cam.login():
-	print(host_ip, "OK")
-else:
-	print("FAIL")
+    with open("config.json") as fp:
+        auth = load(fp)
+    cam = DVRIPCam(host_ip, user=auth["user"], password=auth["password"])
+    if cam.login():
+        stdscr.addstr(0, 0, f"{host_ip} OK\n")
+    else:
+        stdscr.addstr(0, 0, "FAIL\n")
+        stdscr.refresh()
+        sleep(2)
+        return
 
-key_dict = {
-    "u": "Up",
-    "d": "Down",
-    "r": "Left",
-    "l": "Right"
-}
-while True:
-        a = input()
-        dir, duration = a[0], a[1:]
-        if dir in key_dict.keys():
-                dir = key_dict[dir]
-                if dir in ["Up", "Down"]:
-                    duration = int(duration) / 3
-                cam.ptz(f"Direction{dir}", step=5, preset=0)
-                sleep(int(duration) / 10)
-                cam.ptz(f"Direction{dir}", step=5, preset=-1)
+    key_dict = {
+        curses.KEY_UP: "Up",
+        curses.KEY_DOWN: "Down",
+        curses.KEY_LEFT: "Right",
+        curses.KEY_RIGHT: "Left"
+    }
+
+    stdscr.nodelay(True)
+    stdscr.timeout(100)
+
+    stdscr.addstr(1, 0, "Use as setas para controlar a câmera. q para sair.\n")
+    stdscr.refresh()
+
+    while True:
+        key = stdscr.getch()
+        if key in key_dict:
+            direction = key_dict[key]
+            stdscr.addstr(2, 0, f"{direction} pressionada  ")
+            stdscr.refresh()
+
+            cam.ptz(f"Direction{direction}", step=5, preset=0)
+            sleep(0.3)
+            cam.ptz(f"Direction{direction}", step=5, preset=-1)
+
+        elif key == ord('q'):
+            break
+
+if __name__ == "__main__":
+    curses.wrapper(control_camera)
 
